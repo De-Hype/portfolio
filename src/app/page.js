1ffedac13767e26a37a4f5c64ae3f30e6ae074ae
De-Hype <iamsocialhype@gmail.com>
2024-01-21 03:19:04 -0800

    Corrected aos mistake

--- src/app/page.js
+++ src/app/page.js
@@ -18,35 +18,47 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 export default function Home() {
   useEffect(() => {
-    AOS.init({duration:3000})
-    return () => {
-      
-    }
-  }, [])
-  
- 
-  
+    AOS.init({ duration: 3000 });
+    return () => {};
+  }, []);
+
   return (
     <main className="h-screen flex flex-col relative">
       <section className="w-full h-full flex relative">
         <section className="w-[30%] -z-50 h-full border-r fixed tablet:hidden">
           <Header scrollToRef />
         </section>
-        <section  className="w-[70%] tablet:w-full relative flex flex-col gap-8 h-full px-3 ml-auto tablet:mr-auto">
+        <section className="w-[70%] tablet:w-full relative flex flex-col gap-8 h-full px-3 ml-auto tablet:mr-auto">
           <section className="tablet:block hidden fixed right-1.5 left-1.5 top-1.5 z-50">
-            <FloatingNavigations  />
+            <FloatingNavigations />
           </section>
-          <section className="tablet:block hidden mt-8" data-aos = "fade-up">
+          <section className="tablet:block hidden mt-8" data-aos="fade-up">
             <MobileHero />
           </section>
+          <section data-aos="fade-up">
+            <About />
+          </section>
 
-          <About  data-aos = "fade-up"/>
-          <Skills  data-aos = "fade-up" />
-          <Experience   data-aos = "fade-up"/>
-          <Projects  data-aos = "fade-up" />
-          <Writings   data-aos = "fade-up"/>
-          <Contact data-aos = "fade-up" />
-          <Footer data-aos = "fade-up" />
+          <section data-aos="fade-up">
+            <Skills />
+          </section>
+          <section data-aos="fade-up">
+            <Experience />
+          </section>
+
+          <section data-aos="fade-up">
+            <Projects />
+          </section>
+
+          <section data-aos="fade-up">
+            <Writings data-aos="fade-up" />
+          </section>
+          <section data-aos="fade-up">
+            <Contact />
+          </section>
+          <section data-aos="fade-up">
+            <Footer />
+          </section>
         </section>
       </section>
     </main>
