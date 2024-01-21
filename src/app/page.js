454c687217cc87da6039694d4a1bcfc32c97cd90
De-Hype <iamsocialhype@gmail.com>
2024-01-21 06:57:10 -0800

    Cleaned up the final section ref that was left

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import MobileHero from "@/Components/MobileHero";
 import FloatingNavigations from "@/Components/FloatingNavigations";
 import { Skills } from "@/Components/Skills";
 import { Writings } from "@/Components/Writings";
-import useSmoothScroll from "@/lib/SectionRef";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
@@ -25,7 +25,7 @@ export default function Home() {
     <main className="h-screen flex flex-col relative">
       <section className="w-full h-full flex relative">
         <section className="w-[30%] -z-50 h-full border-r fixed tablet:hidden">
-          <Header scrollToRef />
+          <Header />
         </section>
         <section className="w-[70%] tablet:w-full relative flex flex-col gap-8 h-full px-3 ml-auto tablet:mr-auto">
           <section className="tablet:block hidden fixed right-1.5 left-1.5 top-1.5 z-50">
