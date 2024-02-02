394b2d8acfc4ff7730ecd879e9ce3f4210a85fac
David Hype <iamsocialHype@gmail.com>
2024-02-02 08:41:29 +0100

    Update About.jsx, by adding tags for readability

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,8 +8,10 @@ const About = () => {
         <article className=" text-sm">
           I&apos;m a passionate Fullstack developer with a strong proficiency in
           HTML, CSS, TailwindCSS, React.js, Next.js, Svelte, Node.js,
-          Express.js, Typescript, and good ol&apos; Javascript. With a keen eye for
-          clean code, and a knack for creating interactive interfaces,
+          Express.js, Typescript, and good ol&apos; Javascript.
+        </article>
+        <article className=" text-sm">
+          With a keen eye for clean code, and a knack for creating interactive interfaces,
           responsive and scalable web applications. I thrive on turning ideas
           into reality
         </article>
@@ -21,8 +23,8 @@ const About = () => {
         </article>
         <article className=" text-sm">
           Let&apos;s make a mark in the digital landscape, by connecting,
-          collaborating, and bringing out outstanding web projects to life. If
-          you have an exciting project or just want to chat about the latest in
+          collaborating, and bringing out outstanding web projects to life.
+          If you have an exciting project or just want to chat about the latest in
           web development, and software engineering, feel free to reach out.
           Together, we can make the web a better place, one line of code at a
           time. And of course, with Naruto showing on the screen 😹🍿
